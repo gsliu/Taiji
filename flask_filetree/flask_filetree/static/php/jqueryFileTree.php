3f0a8d7ffee81195cba23f943a418b4320446fc6
xinda yan <yanx@vmware.com>
2015-08-03 01:07:02 -0700

    add flask file tree

---
+++ flask_filetree/flask_filetree/static/php/jqueryFileTree.php
@@ -0,0 +1,20 @@
+<ul class='jqueryFileTree'>
+	<li class='file ext_html'>
+		<a href='#' rel='/index.html'>index.html</a>
+	</li>
+	<li class='file ext_php'>
+		<a href='#' rel='/jqueryFileTree.php'>jqueryFileTree.php</a>
+	</li>
+	<li class='directory collapsed'>
+		<a href='#' rel='/media/'>media</a>
+	</li>
+	<li class='directory collapsed'>
+		<a href='#' rel='/pictures/'>pictures</a>
+	</li>
+	<li class='file ext_md'>
+		<a href='#' rel='/README.md'>README.md</a>
+	</li>
+	<li class='directory collapsed'>
+		<a href='#' rel='/webfiles/'>webfiles</a>
+	</li>
+</ul>
